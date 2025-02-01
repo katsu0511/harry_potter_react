cc90e5ee80773419cf2c1e13b0a089d3ad1bf3dc
Katsuya <haradakatsuya190511@gmail.com>
2025-02-01 14:26:24 -0800

    fetch info by react query

--- src/HarryPotter.js
+++ src/HarryPotter.js
@@ -1,7 +1,18 @@
+import { useQuery } from 'react-query';
+
+const fetchCharacters = async () => {
+  const res = await fetch(`https://hp-api.onrender.com/api/characters`);
+  if (res.ok){ return res.json(); }
+  throw new Error(res.statusText);
+};
+
 export default function HarryPotter() {
+
+  const { data } = useQuery('weather', fetchCharacters);
+
   return (
     <div>
-      <h1>HarryPotter</h1>
+      <p>data</p>
     </div>
   );
 }
