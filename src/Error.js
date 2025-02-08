bd95929f12338a1cd16bcd12d13a886096db3830
Katsuya <haradakatsuya190511@gmail.com>
2025-02-08 15:28:38 -0800

    add Error.js

---
+++ src/Error.js
@@ -0,0 +1,12 @@
+export default function Error() {
+  return (
+    <main>
+      <header>
+        <h1>Harry Potter characters</h1>
+      </header>
+      <section className='error'>
+        <p>Error has happened.</p>
+      </section>
+    </main>
+  );
+}
