4043d2247b0fe246120c206639773aa7cd8e9f59
Katsuya <haradakatsuya190511@gmail.com>
2025-02-08 15:20:24 -0800

    add Loading.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ErrorBoundary } from 'react-error-boundary';
 import HarryPotter from './HarryPotter';
+import Loading from './Loading';
 
 const cli = new QueryClient({
   defaultOptions: {
@@ -13,7 +14,7 @@ const cli = new QueryClient({
 
 function App() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<Loading />}>
       <ErrorBoundary fallback={<div>Error has happened.</div>}>
         <QueryClientProvider client={cli}>
           <HarryPotter />
---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+export default function Loading() {
+  return (
+    <main>
+      <header>
+        <h1>Harry Potter characters</h1>
+      </header>
+      <section className='characters'>
+        <p>Loading...</p>
+      </section>
+    </main>
+  );
+}
